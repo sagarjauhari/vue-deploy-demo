51b99ad769db40bc156ca2f478f06a87b8a58e6b
Sagar Jauhari <sagarjauhari@gmail.com>
2016-05-17 20:42:53 -0400

    fix quotes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require(‘express’);
 var path = require(‘path’);
-var serveStatic = require(‘serve-static’)
+var serveStatic = require(‘serve-static’);
 app = express();
 app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
